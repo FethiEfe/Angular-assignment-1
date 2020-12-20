3ce84b6efc3cb5146a65b20caec4600a5531b0bf
FETHI AKCAY <f.akcay1@gmail.com>
2020-12-20 16:38:43 -0500

    Create angular components and display them in app comp

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core"
+
+@Component({
+    selector: "[app-success]",
+    template: `
+        <div class="alert alert-success"> I am success</div>
+    `
+})
+export class SuccessComponent{
+
+}
